5f3e84e6df6e14f8982172f20cf2143723824c0b
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2017-04-27 16:12:17 +0800

    Initial commit

---
+++ wincoala/DataModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wincoala
+{
+    public struct BearMetadata
+    {
+        public String name;
+        public String desc;
+        public List<String> languages;
+    }
+
+    public struct LintRequest
+    {
+        public List<String> bears;
+        public String fileData;
+    }
+}
---
+++ wincoala/wincoalaCore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace wincoala
+{
+    public class WincoalaCore
+    {
+        private HttpClient apiClient;
+
+        public WincoalaCore()
+        {
+            this.apiClient = new HttpClient(
+                new HttpClientHandler
+                {
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+                });
+            this.apiClient.BaseAddress = new Uri("https://webservices.coala.io/");
+        }
+
+        public List<BearMetadata> getBearList()
+        {
+            HttpResponseMessage response = this.apiClient.GetAsync("list/bears").Result;
+            response.EnsureSuccessStatusCode();
+            string resultAsString = response.Content.ReadAsStringAsync().Result;
+                
+            // Convert JSON -> Dict<bearName, BearMetadata> -> List<BearMetadata>
+            Dictionary<String, BearMetadata> bearsData = 
+                JsonConvert.DeserializeObject<Dictionary<String, BearMetadata>>(resultAsString);
+            List<BearMetadata> result = new List<BearMetadata>();
+            foreach(KeyValuePair<String, BearMetadata> bear in bearsData) {
+                BearMetadata newBear = bear.Value;
+                newBear.name = bear.Key;
+                result.Add(newBear);
+            }
+
+            return result;
+        }
+    }
+}
